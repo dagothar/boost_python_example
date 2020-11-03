102c756cda286925f420ffc7fe7ed3cb56f0ba86
dagothar <dagothar@gmail.com>
2020-11-03 13:08:06 +0100

    done

--- src/example.cpp
+++ src/example.cpp
@@ -9,26 +9,19 @@ namespace python = boost::python;
 namespace fs = boost::filesystem;
 
 int main(int argc, char* argv[]) {
-  fs::path exe_path(argv[0]);
-  cout << exe_path << endl;
-  fs::path exe_dir = exe_path.parent_path();
-  cout << exe_dir << endl;
+  string exe_dir = fs::read_symlink("/proc/self/exe").parent_path().string();
   
-  string dir = fs::read_symlink("/proc/self/exe").parent_path().string();
-  cout << dir << endl;
+  string module_path = exe_dir + "/../scripts";
+  cout << module_path << endl;
   
-  //setenv("PYTHONPATH", ".", 1);
-  //PyRun_SimpleString("import sys\nsys.path.append(\"/home/dagothar/test/pyboost/build\")");
   Py_Initialize();
-  //PyRun_SimpleString("import sys\nsys.path.append(\"/home/dagothar/test/pyboost/build\")");
-  PyRun_SimpleString(("import sys\nsys.path.append(\"" + dir + "\")").c_str());
+  PyRun_SimpleString(("import sys\nsys.path.append(\"" + module_path + "\")").c_str());
   
-  python::object my_module = python::import("sample_module");
-  python::object foo = my_module.attr("sample_function");
-  python::object res = foo(5.0);
-  float y = python::extract<float>(res);
+  python::object sample_module = python::import("sample_module");
+  python::object sample_function = sample_module.attr("sample_function");
+  python::object result = sample_function(5.0);
+  float y = python::extract<float>(result);
   cout << y << endl;
   
-  //Py_Finalize();
   return 0;
 }
